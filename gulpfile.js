02d921e749b6f95d2325d21819374de7191b2ec1
Geoff Doty <n2geoff@gmail.com>
2016-05-12 01:10:22 -0400

    fixed dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@
 var gulp = require('gulp');
 var minify = require('gulp-clean-css');
 var rename = require('gulp-rename');
+var concat = require('gulp-concat');
 var myth = require('gulp-myth');
 
 // create task
 gulp.task('default', function(){
     gulp.src('src/*.css')
         .pipe(myth())
+        .pipe(concat('layout.css'))
         .pipe(gulp.dest('./dist'))
         .pipe(minify())
         .pipe(rename('layout.min.css'))
