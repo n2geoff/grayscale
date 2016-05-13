f9bdb862d46331b8b712bcd5d3652ec7c342021e
Geoff Doty <n2geoff@gmail.com>
2016-05-13 00:12:55 -0400

    name change again

--- gulpfile.js
+++ gulpfile.js
@@ -9,10 +9,10 @@ var myth = require('gulp-myth');
 // create task
 gulp.task('default', function(){
     gulp.src('src/*.css')
-        .pipe(concat('graymatter.css'))
+        .pipe(concat('grayscale.css'))
         .pipe(myth())
         .pipe(gulp.dest('./dist'))
         .pipe(minify())
-        .pipe(rename('graymatter.min.css'))
+        .pipe(rename('grayscale.min.css'))
         .pipe(gulp.dest('./dist'))
 });
